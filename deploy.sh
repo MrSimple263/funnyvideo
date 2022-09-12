c2dbec17ec1f218fa79965d975365593c18e090a
Minh Tran <quangminhtran26@gmail.com>
2022-09-13 00:05:50 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 echo "$PWD"
+rm rm -rf back-end/public/img
+rm rm -rf back-end/public/index_bundle.js
 cd front-end
 echo "$PWD"
 npm run build
 cp -r ./public/img ../back-end/public/
-cp ./bundle/index_bundle.js ../back-end/public/
+cp -r ./bundle/index_bundle.js ../back-end/public/
+git add back-end/public/*
+now=$(date +"%T")
+git commit -m "deploy $now"
+git subtree push --prefix back-end heroku master
