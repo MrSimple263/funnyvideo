972db1bf87f14a3ea7aa41cbf8582c40b2bbab6a
Minh Tran <quangminhtran26@gmail.com>
2022-09-13 12:31:30 +0700

    deal 404 not found

--- back-end/server.js
+++ back-end/server.js
@@ -8,8 +8,8 @@ const userRouter = require('./route/user')
 const videoRouter = require('./route/video')
 
 app.use(cors())
-console.log('url',process.env.URL_DB)
-console.log('token',process.env.ACCESS_TOKEN_SECRET)
+console.log('url', process.env.URL_DB)
+console.log('token', process.env.ACCESS_TOKEN_SECRET)
 mongoose.connect(process.env.URL_DB, () => {
     console.log('DB connected')
 })
@@ -17,12 +17,12 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use('/user', userRouter)
 app.use('/video', videoRouter)
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 //deal 404 not found
 app.use(function (req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.status(404).send("<h1>Page not found on the server</h1>")
 });
 
 app.listen(process.env.PORT || '3000')
